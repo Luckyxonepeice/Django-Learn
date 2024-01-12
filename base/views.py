1a5438432fae4b2fdef26ebdfbb59516b890ef64
Luckyxonepeice <lakshaychauhan129@gmail.com>
2024-01-13 03:37:06 +0530

    Chat room CRUD

--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.db.models import Q
-from .models import Room,Topic
+from .models import Room,Topic,Message
 from .forms import RoomForm
 from django.contrib.auth.models import User 
 from django.contrib import messages
@@ -8,6 +8,7 @@ from django.contrib.auth import authenticate , login , logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+
 
 def loginPage(request):
     
@@ -75,7 +76,22 @@ def home(request):
 def room(request,pk):
     
     room = Room.objects.get(id=pk)
-    context = {'room':room}
+    room_messages= room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+    
+    if request.method =='POST':
+        message= Message.objects.create(
+            user = request.user,
+            room = room ,
+            body = request.POST.get('body')
+        )
+        
+        room.participants.add(request.user)
+        
+        return redirect('room', pk=room.id)
+    
+    
+    context = {'room':room, "room_messages":room_messages, "participants":participants}
     return render(request,'base/room.html',context)
 
 
